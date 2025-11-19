16e6b4bc8199579abf626e8cb01313bee0596a34
haon02 <noahvad@gmail.com>
2025-11-19 10:24:47 +0100

    Lavet constructor i Udflugt & Konference + Dobbeltrettet 0..1-til-mange komposition mellem Konference og Udflugt

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,4 @@
-package cotroller;
+package controller;
 
 public class Controller {
 }
--- src/model/Konference.java
+++ src/model/Konference.java
@@ -1,4 +1,44 @@
 package model;
 
+import javax.management.Attribute;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Konference {
+    private String navn;
+    private String sted;
+    private LocalDate startDato;
+    private LocalDate slutDato;
+    private int dagsPris;
+    private ArrayList<Tilmelding> tilmeldinger = new ArrayList<>();
+    private ArrayList<Udflugt> udflugter = new ArrayList<>();
+    private ArrayList<Hotel> hoteller = new ArrayList<>();
+    private Adminstrator adminstrator;
+
+    public Konference(String navn, String sted, LocalDate startDato, LocalDate slutDato, int dagsPris, ArrayList<Tilmelding> tilmeldinger, ArrayList<Udflugt> udflugter, ArrayList<Hotel> hoteller, Adminstrator adminstrator) {
+        this.navn = navn;
+        this.sted = sted;
+        this.startDato = startDato;
+        this.slutDato = slutDato;
+        this.dagsPris = dagsPris;
+        this.tilmeldinger = tilmeldinger;
+        this.udflugter = udflugter;
+        this.hoteller = hoteller;
+        this.adminstrator = adminstrator;
+    }
+
+    public ArrayList<Udflugt> getUdflugter() {
+        return udflugter;
+    }
+
+    public Udflugt createUdflugt(String udflugtName){
+        Udflugt udflugt = new Udflugt("Udflugt test",200,LocalDate.now(), new ArrayList<>(),this);
+        udflugter.add(udflugt);
+        return udflugt;
+    }
+    public void removeUdflugt(Udflugt udflugt){
+        if (udflugter.contains(udflugt)){
+            udflugter.remove(udflugt);
+        }
+    }
 }
