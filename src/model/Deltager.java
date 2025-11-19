ce27624c2ad2b5b11b2ea7790c98cc290fef1faf
rasjo <rasmus.joergensen@outlook.com>
2025-11-19 10:32:32 +0100

    Ændret "Deltager+Firma"

--- src/model/Deltager.java
+++ src/model/Deltager.java
@@ -1,4 +1,65 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Deltager {
+    private String navn;
+    private String adresse;
+    private String nationalitet;
+    private String email;
+    private String telefon;
+    private boolean erForedragsholder;
+
+    private final ArrayList<Tilmelding> tilmeldinger= new ArrayList<>();
+
+
+    public Deltager(String navn, String adresse, String nationalitet, String email, String telefon, boolean erForedragsholder) {
+        this.navn = navn;
+        this.adresse = adresse;
+        this.nationalitet = nationalitet;
+        this.email = email;
+        this.telefon = telefon;
+        this.erForedragsholder = erForedragsholder;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getNationalitet() {
+        return nationalitet;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public boolean isErForedragsholder() {
+        return erForedragsholder;
+    }
+
+    public ArrayList<Tilmelding> getTilmeldinger() {
+        return new ArrayList<>(tilmeldinger);
+    }
+
+    public void addTilmelding(Tilmelding tilmelding) {
+        if (!tilmeldinger.contains(tilmelding)) {
+            tilmeldinger.add(tilmelding);
+        }
+    }
+
+    public void removeTilmelding(Tilmelding tilmelding) {
+        if (tilmeldinger.contains(tilmelding)) {
+            tilmeldinger.remove(tilmelding);
+
+        }
+    }
 }
--- src/model/Firma.java
+++ src/model/Firma.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Firma {
+
+    private String navn;
+    private String telefon;
+
+    public Firma(String navn, String telefon) {
+        this.navn = navn;
+        this.telefon = telefon;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
 }
