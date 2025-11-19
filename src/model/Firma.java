ac746bfbb8f651ece88446e9898544e376749d81
rasjo <rasmus.joergensen@outlook.com>
2025-11-19 11:17:41 +0100

    Ændret "Firma" og "Deltager"

--- src/model/Firma.java
+++ src/model/Firma.java
@@ -1,14 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Firma {
 
     private String navn;
     private String telefon;
 
+    private final ArrayList<Deltager> deltagere = new ArrayList<>();
+
     public Firma(String navn, String telefon) {
         this.navn = navn;
         this.telefon = telefon;
     }
+
+    public ArrayList<Deltager> getDeltagere() {
+        return new ArrayList<>(deltagere);
+    }
+
+    public void addDeltager(Deltager deltager) {
+        if (!deltagere.contains(deltager)) {
+            deltagere.add(deltager);
+            deltager.setFirma(this);
+        }
+    }
+
+    public void removeDeltager(Deltager deltager) {
+        if (deltagere.contains(deltager)) {
+            deltagere.remove(deltager);
+            deltager.setFirma(this);
+        }
+    }
+
 
     public String getNavn() {
         return navn;
