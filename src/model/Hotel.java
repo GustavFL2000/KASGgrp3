324b136cfe05f4f9fedcd8dd25b5b9bc3c5d2dec
Luca <lucavadstruppetersen@gmail.com>
2025-11-19 10:27:53 +0100

    hotel indhold

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -1,4 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String name;
+    private String adresse;
+    private String beskrivelse;
+    private ArrayList<Service> services = new ArrayList<>();
+    private ArrayList<HotelReservation> reservations = new ArrayList<>();
+
+    public Hotel(String name, String adresse, String beskrivelse) {
+        this.name = name;
+        this.adresse = adresse;
+        this.beskrivelse = beskrivelse;
+    }
+
+    public Service createService(Boolean wifi, Boolean mad, Boolean bad) {
+        Service service = new Service(wifi, mad, bad);
+        services.add(service);
+        return service;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public ArrayList<Service> getServices() {
+        return services;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "name='" + name + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", beskrivelse='" + beskrivelse + '\'' +
+                ", services=" + services +
+                ", reservations=" + reservations +
+                '}';
+    }
 }
--- src/model/Service.java
+++ src/model/Service.java
@@ -1,4 +1,34 @@
 package model;
 
 public class Service {
+    private Boolean wifi;
+    private Boolean bad;
+    private Boolean mad;
+
+    public Service(Boolean wifi, Boolean bad, Boolean mad) {
+        this.wifi = wifi;
+        this.bad = bad;
+        this.mad = mad;
+    }
+
+    public Boolean hasWifi() {
+        return wifi;
+    }
+
+    public Boolean hasBad() {
+        return bad;
+    }
+
+    public Boolean hasMad() {
+        return mad;
+    }
+
+    @Override
+    public String toString() {
+        return "Service{" +
+                "wifi=" + wifi +
+                ", bad=" + bad +
+                ", mad=" + mad +
+                '}';
+    }
 }
