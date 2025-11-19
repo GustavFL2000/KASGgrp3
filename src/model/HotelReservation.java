9c9eb73d5186e8245ba472cfe3917d799787503a
haon02 <noahvad@gmail.com>
2025-11-19 11:59:03 +0100

    Hotelreservation -- Tilmelding, done

--- src/model/HotelReservation.java
+++ src/model/HotelReservation.java
@@ -1,4 +1,19 @@
 package model;
 
 public class HotelReservation {
+    private boolean isDoubleRoom;
+    private double pris;
+    private Hotel hotel;
+    private Tilmelding tilmelding;
+
+    public HotelReservation(boolean isDoubleRoom, double pris, Hotel hotel, Tilmelding tilmelding) {
+        this.isDoubleRoom = isDoubleRoom;
+        this.pris = pris;
+        this.hotel = hotel;
+        this.tilmelding = tilmelding;
+    }
+
+    public Tilmelding getTilmelding() {
+        return tilmelding;
+    }
 }
--- src/model/Tilmelding.java
+++ src/model/Tilmelding.java
@@ -1,4 +1,40 @@
 package model;
 
+import java.sql.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Tilmelding {
+    private int AntalDage;
+    private LocalDate ankomstDato;
+    private LocalDate afrejseDato;
+    private int totalPris;
+    private Konference konference;
+    private Deltager deltager;
+    private ArrayList<HotelReservation> hotelReservationer = new ArrayList<>();
+
+    public Tilmelding(int antalDage, LocalDate ankomstDato, LocalDate afrejseDato, int totalPris, Konference konference, Deltager deltager) {
+        AntalDage = antalDage;
+        this.ankomstDato = ankomstDato;
+        this.afrejseDato = afrejseDato;
+        this.totalPris = totalPris;
+        this.konference = konference;
+        this.deltager = deltager;
+    }
+
+    public ArrayList<HotelReservation> getHotelReservation() {
+        return hotelReservationer;
+    }
+
+    public HotelReservation createHotelreservation(boolean isDoubleRoom, double pris, Hotel hotel, Tilmelding tilmelding) {
+        HotelReservation hotelReservation = new HotelReservation(isDoubleRoom, pris, hotel, tilmelding);
+        hotelReservationer.add(hotelReservation);
+        return hotelReservation;
+    }
+
+    public void removeHotelReservation(HotelReservation hotelReservation) {
+        if (hotelReservationer.contains(hotelReservation)){
+            hotelReservationer.remove(hotelReservation);
+        }
+    }
 }
